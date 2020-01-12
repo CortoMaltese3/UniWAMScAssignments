a2bb9e9900e624f6aae798ea3017f972f6c940f2
Giorgos Kalomalos <Giorgos.Kalomalos@Peoplecert.onmicrosoft.com>
2020-01-12 14:47:17 +0200

    Finished simplified Q6
    Added Q4

--- 102 - Αρχές ψηφιακής τεχνολογίας/SemesterAssignment/SemesterAssignmentSimplifiedQ6/ProgramQ6.cs
+++ 102 - Αρχές ψηφιακής τεχνολογίας/SemesterAssignment/SemesterAssignmentSimplifiedQ6/ProgramQ6.cs
@@ -7,126 +7,103 @@ namespace SemesterAssignmentSimplifiedQ6
     class ProgramQ6
     {
         static void Main(string[] args)
-        {
-            //var function = "(A AND B) OR C";
-            Console.WriteLine("Enter function: ");
+        {            
+            Console.Write("Enter function: ");
             var userFunction = Console.ReadLine();
 
             while (!IsValidLogicalFunction(userFunction.ToUpper()))
             {
-                Console.WriteLine("Invalid function. Each input needs to appear once and the available gates are AND, OR, NOT. Enter function: ");
+                Console.Write("Invalid function. Each input needs to appear once and the available gates are AND, OR, NOT. Enter function: ");
                 userFunction = Console.ReadLine();
             }
-
-            var TruthTableTwoInputs = new List<string>()
-            {
-                "00", "01", "10", "11"
-            };
 
             var TruthTableThreeInputs = new List<string>()
             {
                 "000", "001", "010", "011", "100", "101", "110", "111"
             };
 
+            var functionOutcome = String.Empty;
 
+            Console.WriteLine("Truth table is: \n");
+
+            for (int index  = 0; index < TruthTableThreeInputs.Count(); index++)
+            {
+                Console.WriteLine(TruthTableThreeInputs[index] + " " + CalculateTruthTable(userFunction.ToUpper())[index]);
+            }
         }
 
-
-
-        static void ConvertToTruthTable(string userInput)
+        static List<string> CalculateTruthTable(string userInput)
         {
-            var truthTable = new List<string>();
-            var parenthesisValues = new List<string>();
-            var words = new List<string>();
+            var truthTable = String.Empty;            
+            var userInputToWords = new List<string>();
+            var truthTableList = new List<string>();
 
-            foreach (var word in userInput)
+            foreach (var word in userInput.Split(" "))
             {
-
+                userInputToWords.Add(word);
             }
 
-
-            foreach (var character in userInput)
-            {                
-                if (userInput.Contains("("))
+            if (userInputToWords.Count(w => w == "AND") > 1)
+            {
+                truthTable = "00000001";
+            }
+            if (userInputToWords.Count(w => w == "OR") > 1)
+            {
+                truthTable = "01111111";
+            }
+            if (userInput.IndexOf("AND") < userInput.IndexOf("OR"))
+            {
+                if (userInput.Contains("(") && userInput.IndexOf("(") != 0)
                 {
-                    var parenthesisStart = userInput.IndexOf("(");
-                    var parenthesisEnd = userInput.IndexOf(")");
-                    var parenthesisWords = userInput.Substring(parenthesisStart + 1, parenthesisEnd - parenthesisStart - 1).Split(" ");
-                    var parenthesisList = new List<string>();
-
-                    if (parenthesisWords.Contains("AND"))
-                    {
-
-                    }
-
-                    foreach (var word in parenthesisWords)
-                    {
-                        parenthesisList.Add(word);
-                    }
-
-                    if (parenthesisList.Contains("AND"))
-                    {
-
-                         
-
-                    }
-
-                    //deals with the calculations in parenthesis
-                    foreach (var item in parenthesisList)
-                    {
-                        parenthesisValues.Add(item.ToString());                        
-                    }
-                    if (parenthesisValues.Contains("AND"))
-                    {
-
-                    }
+                    truthTable = "00000111";
                 }
-
+                else
+                {
+                    truthTable = "01010111";
+                }                
             }
+            if (userInput.IndexOf("AND") > userInput.IndexOf("OR"))
+            {
+                if (userInput.Contains("(") && userInput.IndexOf("(") != 0)
+                {
+                    truthTable = "00011111";
+                }
+                else
+                {
+                    truthTable = "00010101";
+                }
+            }
+            foreach (var character in truthTable)
+            {
+                truthTableList.Add(character.ToString());
+            }
+            return truthTableList;
         }
 
         static bool IsValidLogicalFunction(string userInput)
         {
-            var checkA = userInput.Replace("A", "*");
-            if (checkA.Count(a => a == '*') > 1 || userInput.Count(b => b == 'B') > 1 || userInput.Count(c => c == 'C') > 1)
+            var userInputToWords = new List<string>();
+            var listOfValidInputs = new List<string>()
+            {
+                "AND", "OR", "NOT", "A", "B", "C"
+            };
+
+            userInput.Replace("(", "").Replace(")", "");
+
+            foreach (var word in userInput.Split(" "))
+            {
+                if (!listOfValidInputs.Contains(word))
+                {
+                    return false;
+                }
+                userInputToWords.Add(word);
+            }
+
+            if (userInputToWords.Count(a => a == "A") > 1 || userInputToWords.Count(b => b == "B") > 1 || userInputToWords.Count(c => c == "C") > 1)
             {
                 return false;
             }
-            //if (listOfValidInputs.Contains(userInput.ToUpper()))
-            //{
-            //    return true;
-            //}
-            else
-            {
-                return true;
-            }
-        }
-
-        static void CalculateFunction(string userInput, List<string> possibleInputs)
-        {
-            var subTruthTable = new List<string>();
-            if (userInput.Contains("AND"))
-            {
-                foreach (var inputSet in possibleInputs)
-                {
-                    if (inputSet.Contains("0"))
-                    {
-                        subTruthTable.Add("0");
-                    }
-                    else
-                    {
-                        subTruthTable.Add("1");
-                    }
-                }
-            }
-            if (userInput.Contains("OR"))
-            {
-
-            }
-            //if (userInput.Contains("NOT"))
-            //{
-
-            //}
+            return true;
         }
     }
 }
